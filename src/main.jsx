659e0ff1fa7ff7d6077145a8a1b95b66e6138156
Brian Ladner <git@theladners.com>
2025-09-07 17:50:56 -0500

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false, // default: true
-    },
-  },
-})
-
 createRoot(document.getElementById('root')).render(
-  // <StrictMode>
-    <QueryClientProvider client={queryClient}>
+  <StrictMode>
       <App />
-    </QueryClientProvider>
-  // </StrictMode>
+  </StrictMode>
 )
